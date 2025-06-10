8e9eeaf3e06daded9f5d29c93e1346a5714724cc
taiki0703 <taiki.ida7@gmail.com>
2025-06-11 08:04:55 +0900

    2025/06/11

--- ABC408/C-Not_All_Covered.py
+++ ABC408/C-Not_All_Covered.py
@@ -1,13 +1,36 @@
+## N , M = map(int,input().split())
+
+## Battery = [0] * N
+
+## for i in range(M):
+##     L , R = map(int,input().split())
+##     for i in range(N):
+##         if(L-1 <= i and i <= R-1):
+##             Battery[i] += 1
+
+## print(min(Battery))
+
+## タイムエラー
+## 計算量がO(MN)となり膨大
+## imos法により解決する
+
 N , M = map(int,input().split())
 
-Battery = [0] * N
+Battery = [0] * (N + 1)
+
+count = [0] * (N + 1)
 
 for i in range(M):
     L , R = map(int,input().split())
-    for i in range(N):
-        if(L-1 <= i and i <= R-1):
-            Battery[i] += 1
+    Battery[L-1] += 1
+    Battery[R] -= 1
 
-print(min(Battery))
+for i in range(N + 1):
+    if(i == 0):
+        count[0] = Battery[0]
+    if(i > 0):
+        count[i] = count[i-1] + Battery[i]
 
-##タイムエラー
+count.pop(N)
+
+print(min(count))
